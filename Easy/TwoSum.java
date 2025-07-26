b31197154cb5e037787e6856249b8bcd2ef8608e
soformidable <so_formidable@outlook.com>
2025-07-26 17:35:15 +0530

    mod:TwoSum

--- Easy/TwoSum.java
+++ Easy/TwoSum.java
@@ -11,7 +11,37 @@ public class TwoSum{
         }
         return (new int[]{});
     }
+    public static int[] twoSumHash(int nums[],int target){
+        HashMap<Integer,Integer> hs = new HashMap<Integer,Integer>();
+        for(int i = 0 ; i < nums.length ; i++){
+            int num = nums[i];
+            int diff = target - num;
+            if(hs.containsKey(diff))
+                return(new int[]{i,hs.get(diff)});
+            else
+                hs.put(num,i);
+        }
+        return(new int[]{-1,-1});
+    }
+
+    //Only works for the variant where answer is boolean (Yes/No)
+    public static int[] twoSumTwoPoint(int nums[] , int target){
+        Arrays.sort(nums);
+        int start = 0 , end = nums.length - 1;
+        while(start < end){
+            if(nums[end] + nums[start] == target)
+                return(new int[]{start,end});
+            else if(nums[end] + nums[start] < target)
+                end--;
+            else
+                start++;
+        }
+        return (new int[]{-1,-1});
+    }
+
     public static void main(String args[]){
-        System.out.println(Arrays.toString(twoSum(new int[]{2,7,11,15}, 9)));
+        System.out.println(Arrays.toString(twoSum(new int[]{3,3}, 6)));
+        System.out.println(Arrays.toString(twoSumHash(new int[]{3,3}, 6)));
+        System.out.println(Arrays.toString(twoSumTwoPoint(new int[]{3,2,4}, 6)));
     }
 }
